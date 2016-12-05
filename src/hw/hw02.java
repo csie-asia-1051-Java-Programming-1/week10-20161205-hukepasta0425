e1bd4bc401fc5fdd4b755a349353d90e3019e772
hukepasta0425 <105021031@live.asia.edu.tw>
2016-12-05 16:22:37 +0800

    12/05
    go

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,28 @@
 package hw;
 
+import java.util.*;
+
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner src = new Scanner(System.in);
+		int data[] = new int[4];
+		int max = 0;
+		for (int i = 0; i < 4; i++) {
+			data[i] = src.nextInt();
+		}
+		int a = data[0];
+		int b = data[1];
+		int c = data[2];
+		int d = data[3];
+		if (a == b || b == c || c == d || a == d) {
+			for (int i = 0; i < 4; i++) {
+				if (data[i] > max) {
+					max = data[i];
+				}
+			}
+			System.out.print(max * 2);
+		}
 	}
-
 }
